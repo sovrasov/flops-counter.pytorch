9df4db4963cdeca5221471659b8449e4c0039702
Vladislav Sovrasov <sovrasov.vlad@gmail.com>
2019-04-09 16:45:07 +0300

    Add an ability to create custom inputs for models

--- ptflops/flops_counter.py
+++ ptflops/flops_counter.py
@@ -2,13 +2,18 @@ import torch.nn as nn
 import torch
 import numpy as np
 
-def get_model_complexity_info(model, input_res, print_per_layer_stat=True, as_strings=True):
+def get_model_complexity_info(model, input_res, print_per_layer_stat=True, as_strings=True,
+                              input_constructor=None):
     assert type(input_res) is tuple
     assert len(input_res) == 2
-    batch = torch.FloatTensor(1, 3, *input_res)
     flops_model = add_flops_counting_methods(model)
     flops_model.eval().start_flops_count()
-    out = flops_model(batch)
+    if input_constructor:
+        input = input_constructor(input_res)
+        _ = flops_model(**input)
+    else:
+        batch = torch.FloatTensor(1, 3, *input_res)
+        _ = flops_model(batch)
 
     if print_per_layer_stat:
         print_model_with_flops(flops_model)
