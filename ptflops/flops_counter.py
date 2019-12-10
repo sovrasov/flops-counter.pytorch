817e28a003ab7cb5c2eef3a7f59d1d1559b12b78
Vladislav Sovrasov <sovrasov.vlad@gmail.com>
2019-12-10 13:29:25 +0300

    Update license

--- ptflops/flops_counter.py
+++ ptflops/flops_counter.py
@@ -1,3 +1,11 @@
+'''
+Copyright (C) 2019 Sovrasov V. - All Rights Reserved
+ * You may use, distribute and modify this code under the
+ * terms of the MIT license.
+ * You should have received a copy of the MIT license with
+ * this file. If not visit https://opensource.org/licenses/MIT
+'''
+
 import sys
 
 import torch
--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 
 readme = open('README.md').read()
 
-VERSION = '0.5'
+VERSION = '0.5.1'
 
 requirements = [
     'torch',
