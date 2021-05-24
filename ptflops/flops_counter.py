0414ba7f0065be05cf58b111fc9d42e2b48d77d1
Vladislav Sovrasov <sovrasov.vlad@gmail.com>
2021-05-24 22:25:31 +0300

    Fix possible division by zero

--- ptflops/flops_counter.py
+++ ptflops/flops_counter.py
@@ -104,6 +104,8 @@ def accumulate_flops(self):
 
 def print_model_with_flops(model, total_flops, total_params, units='GMac',
                            precision=3, ost=sys.stdout):
+    if total_flops < 1:
+        total_flops = 1
 
     def accumulate_params(self):
         if is_supported_instance(self):
