f9067f0385185f3191a6327b0cf3f33de6b14f59
Vladislav Sovrasov <vladislav.sovrasov@intel.com>
2023-12-22 10:59:40 +0900

    Fininsh covering of public api with docs

--- ptflops/utils.py
+++ ptflops/utils.py
@@ -10,7 +10,15 @@ Copyright (C) 2021-2023 Sovrasov V. - All Rights Reserved
 from typing import Optional
 
 
-def flops_to_string(flops: int, units: Optional[str] = None, precision: int = 2):
+def flops_to_string(flops: int, units: Optional[str] = None, precision: int = 2) -> str:
+    """
+    Converts integer MACs representation to a readable string.
+
+    :param flops: Input MACs.
+    :param units: Units for string representation of MACs (GMac, MMac or KMac).
+    :param precision: Floating point precision for representing MACs in
+        given units.
+    """
     if units is None:
         if flops // 10**9 > 0:
             return str(round(flops / 10.**9, precision)) + ' GMac'
@@ -31,7 +39,16 @@ def flops_to_string(flops: int, units: Optional[str] = None, precision: int = 2)
             return str(flops) + ' Mac'
 
 
-def params_to_string(params_num: int, units: Optional[str] = None, precision: int = 2):
+def params_to_string(params_num: int, units: Optional[str] = None,
+                     precision: int = 2) -> str:
+    """
+    Converts integer params representation to a readable string.
+
+    :param flops: Input number of parameters.
+    :param units: Units for string representation of params (M, K or B).
+    :param precision: Floating point precision for representing params in
+        given units.
+    """
     if units is None:
         if params_num // 10 ** 6 > 0:
             return str(round(params_num / 10 ** 6, precision)) + ' M'
